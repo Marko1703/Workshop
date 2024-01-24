91c5f8e7c699620cac3507afbea319476bde6fd6
Marko1703 <marko.andonov94@gmail.com>
2024-01-24 18:08:32 +0100

    Updated

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ console.log("Script is working!");
 const personEl = document.querySelector(".person");
 const logoEl = document.querySelector(".logo");
 const shipEl = document.querySelector(".ship");
+const personImgEl = document.querySelector(".personImg");
 
 const People_URL = "https://swapi.dev/api/people/?page=1";
 
@@ -13,20 +14,67 @@ function fetchPeople() {
     })
     .then(function (data) {
       console.log(data);
+
+      renderPeopleTable(personEl, data)
     });
 }
 
+personImgEl.addEventListener("click", function() {
+  fetchPeople(People_URL)
+})
+
 function renderPeopleTable(containerEl, peopleInfo) {
-  let peopleInfoHTML = `
-  <tr>
-    <td>${people.name}</td>
-    <td>${people.height}</td>
-    <td>${people.mass}</td>
-    <td>${people.gender}</td>
-    <td>${people.birth_year}</td>
-    <td>${people.films.length}</td>
-  </tr>
-  `;
+  let tableHTML = ""
+
+  for (let people of peopleInfo.results) {
+    tableHTML += `
+    <tr>
+      <td>${people.name}</td>
+      <td>${people.height}</td>
+      <td>${people.mass}</td>
+      <td>${people.gender}</td>
+      <td>${people.birth_year}</td>
+      <td>${people.films.length}</td>
+    </tr>
+        `;
+  }
+
+  containerEl.innerHTML +=
+`<table class = "mainTable">
+    <thead>
+     <tr>
+         <th>Name</th>
+         <th>Height</th>
+         <th>Mass</th>
+         <th>Gender</th>
+         <th>Birth Year</th>
+         <th>Appearances</th>
+     </tr>
+    </thead>
+    <tbody>${tableHTML}</tbody>
+ </table>
+ <button class="characterNextPage">Previous Page</button>
+ <button class="characterPrevPage">Next Page</button>`;
+
+ const characterNextBtn = document.querySelector(".characterNextPage");
+ const characterPrevBtn = document.querySelector(".characterPrevPage");
+
+ if (!peopleInfo.next) {
+  characterNextBtn.disabled = true
+ }
+
+ if (!peopleInfo.next) {
+  characterPrevBtn.disabled = true
+ }
+
+ characterNextBtn.addEventListener("click", function (){
+  fetchPeople(peopleInfo.next)
+})
+
+characterPrevBtn.addEventListener("click", function (){
+fetchPeople(peopleInfo.previous)
+})
+
 }
 
 fetchPeople();
